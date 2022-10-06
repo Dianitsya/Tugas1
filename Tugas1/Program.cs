20b45aed2080065ba35b102819d098a0ad9f61fe
Dianitsya <dianitasyanandamasta@gmail.com>
2022-10-06 17:14:05 +0700

    method 2

--- Tugas1/Program.cs
+++ Tugas1/Program.cs
@@ -6,4 +6,8 @@ namespace Tugas1
     {
         //Deklarasi array int dengan ukuran 20
         private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
            
